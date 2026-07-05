9651cc468d15edb3a972b542e5c14b32342aed11
samadrita.saha <samadrita.saha@rwth-aachen.de>
2026-07-05 08:21:21 +0200

    Update config.py for JWTs

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
 
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_mins: int = 5
+
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent.parent / ".env",
         extra="ignore",
