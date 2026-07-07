da0fcc674f0db448456abf522918f852ca2c6e21
samadrita.saha <samadrita.saha@rwth-aachen.de>
2026-07-07 06:32:41 +0200

    Add Google Books API Configuration

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 5
     tmdb_read_access_token: str
+    google_books_api_key: str
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent.parent / ".env",
