a30b6ad1313547af3c920d26b834b4a078e478f8
samadrita.saha <samadrita.saha@rwth-aachen.de>
2026-07-19 10:00:35 +0200

    Update Media ORM Model

--- backend/app/models/media.py
+++ backend/app/models/media.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.elements import True_
 
 from app.database.base import Base
 
@@ -20,5 +21,9 @@ class Media(Base):
     external_id: Mapped[str] = mapped_column(String(255), nullable=False)
 
     source: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    creator: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    image: Mapped[str] = mapped_column(String(255), nullable=True)
 
     entries: Mapped[list[Entry]] = relationship(back_populates="media")
